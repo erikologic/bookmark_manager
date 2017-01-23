9b2ef84c22e78db8a0cef6d358a09889de2d8252
Ross Benzie <rossbenzie@Ross-MacBook-Pro.local>
2017-01-23 17:58:04 +0000

    Sinatra showexceptions.rb file hack

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+
+ruby '2.2.3'
+
+gem 'sinatra'
+gem 'rack'
+gem 'rspec-sinatra'
+
+  gem 'rspec'
+  gem 'capybara'
+
+gem 'rake'
---
+++ spec/features/list_of_links_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature 'show list of links' do
+  scenario 'show list of regularly visited sites' do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit('/')
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
+  end
+end
