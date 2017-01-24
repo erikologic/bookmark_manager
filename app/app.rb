96a68253696793ea025607c7e2e253f595d54d6b
Sam Mckay <sammckay12@gmail.com>
2017-01-24 14:41:42 +0000

    created decv environment

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,11 @@
+ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
 #require 'rack/showexceptions'
 require './app/models/link'
 
 class BookmarkManager < Sinatra::Base
+
+  set :environment, :development
 
   get '/links' do
     @links = Link.all
